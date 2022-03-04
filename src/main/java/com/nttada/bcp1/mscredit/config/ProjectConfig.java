ff48b58c0a7f78f9d21445da1244ef2d3f88d88d
MZena <manuelzenareque@gmail.com>
2022-03-04 17:17:59 -0500

    update config client

--- src/main/java/com/nttada/bcp1/mscredit/config/ProjectConfig.java
+++ src/main/java/com/nttada/bcp1/mscredit/config/ProjectConfig.java
@@ -1,7 +1,11 @@
 package com.nttada.bcp1.mscredit.config;
+import lombok.Data;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
+@Data
+@RefreshScope
 @Configuration
 @EnableWebFlux
 public class ProjectConfig {
--- src/main/java/com/nttada/bcp1/mscredit/repository/CreditRepository.java
+++ src/main/java/com/nttada/bcp1/mscredit/repository/CreditRepository.java
@@ -2,7 +2,9 @@ package com.nttada.bcp1.mscredit.repository;
 
 import com.nttada.bcp1.mscredit.modelo.Credit;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CreditRepository extends ReactiveMongoRepository<Credit, String> {
 
 }
